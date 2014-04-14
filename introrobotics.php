a74d301af1a6071d8651f3e52c122f182cac37a6
claytonketner <subwoof@mac.com>
2014-04-14 01:50:11 -0700

    Removed more stupid

--- introrobotics.php
+++ introrobotics.php
@@ -11,7 +11,7 @@
 <div id="all-content-wrapper">
 	<div id="content-wrapper" class="content-sized">
 		<div id="content" class="sidebar-or-content">
-			<h2 id="content-header">Template Header</h2>
+			<h2 id="content-header">Introduction to Robotics (CSCI 445)</h2>
 
 			<h3>Class Summary</h3>
 			<p>In this class, we learned about a bunch of different topics, including:</p> 
