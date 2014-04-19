635cd0ce598cc024f1d246eeac8b2467b307c2fb
claytonketner <subwoof@mac.com>
2014-04-19 15:31:17 -0700

    troubleshooting

--- scripts/javascript/expanding-image.js
+++ scripts/javascript/expanding-image.js
@@ -4,7 +4,7 @@ var showHideSpeed = 400;
 
 $(document).ready(function()
 {
-	$(".expanding-image-wrapper").load(function()
+	$(".expanding-image-wrapper").each().load(function()
 	{
 		prepareImages();
 	});
