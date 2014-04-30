618134ef26389a3e1831c3573876532b6a15f2e0
claytonketner <subwoof@mac.com>
2014-04-30 10:52:06 -0700

    test fix

--- scripts/php/submit.php
+++ scripts/php/submit.php
@@ -17,5 +17,5 @@ $query->bindParam(":name", $name, PDO::PARAM_STR, 30);
 $query->bindParam(":message", $message, PDO::PARAM_STR);
 $query->execute();
 
-header("Location: guestbook.php");
+header("Location: /../guestbook.php");
 return;
